5e726b3d14b2d00d3580231e110402d3615b1045
Léa <leaserrano1119@gmail.com>
2023-01-31 17:46:56 +0100

    Ajout du code de la séance

---
+++ entropie.cpp
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include "image_ppm.h"
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+  char cNomImgLue[250], cNomImgEcrite[250];
+  int nH, nW, nTaille;
+  
+  if (argc != 2) 
+     {
+       printf("Usage: ImageIn.pgm ImageOut.pgm \n"); 
+       exit (1) ;
+     }
+   
+   sscanf (argv[1],"%s",cNomImgLue) ;
+
+   OCTET *ImgIn;
+   
+   lire_nb_lignes_colonnes_image_pgm(cNomImgLue, &nH, &nW);
+   nTaille = nH * nW;
+  
+   allocation_tableau(ImgIn, OCTET, nTaille);
+   lire_image_pgm(cNomImgLue, ImgIn, nH * nW);
+
+   float tabProba[256], tabId[256];
+
+    for (int elt=0; elt < 256; elt++)
+    {
+        tabId[elt] = 0;
+        tabProba[elt] = 0;
+    }
+
+    for (int i=0; i < nH; i++)
+    {
+        for (int j=0; j < nW; j++)
+        {
+            tabId[ImgIn[i*nW+j]] += 1;
+        }
+    }
+
+    for (int k = 0; k < 256; k++) 
+    {
+        tabProba[k] = float(tabId[k]/(nH*nW));
+    }
+
+    float entropie = 0; 
+
+    for (int i = 0; i < 256; i++) {
+        entropie += tabProba[i] * log2(tabProba[i]);
+    }
+
+    entropie *= -1;
+
+    std::cout << "L'entropie est de : " << entropie << " bits/pixel" << std::endl;
+
+
+   free(ImgIn);
+
+   return 1;
+}
---
+++ predictionVoisins1.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include "image_ppm.h"
+
+
+int main(int argc, char* argv[])
+{
+  char cNomImgLue[250], cNomImgEcrite[250];
+  int nH, nW, nTaille;
+  
+  if (argc != 3) 
+     {
+       printf("Usage: ImageIn.pgm ImageOut.pgm \n"); 
+       exit (1) ;
+     }
+   
+   sscanf (argv[1],"%s",cNomImgLue) ;
+   sscanf (argv[2],"%s",cNomImgEcrite);
+
+   OCTET *ImgIn, *ImgTemp, *ImgOut;
+   
+   lire_nb_lignes_colonnes_image_pgm(cNomImgLue, &nH, &nW);
+   nTaille = nH * nW;
+  
+   allocation_tableau(ImgIn, OCTET, nTaille);
+   lire_image_pgm(cNomImgLue, ImgIn, nH * nW);
+   allocation_tableau(ImgTemp, OCTET, nTaille);
+   allocation_tableau(ImgOut, OCTET, nTaille);
+
+   for (int i = 0; i < nH; i++) {
+    for (int j = 0; j < nW; j++) {
+        if (i > 0 && j > 0) {
+            ImgTemp[i*nW+j] = (ImgIn[i*nW+(j-1)] +  ImgIn[(i-1)*nW+j])/2; // A+C/2
+        }
+        else if (i == 0 && j != 0) {
+            ImgTemp[i*nW+j] = ImgIn[i*nW+(j-1)];
+        }
+        else if (j == 0 && i != 0) {
+            ImgTemp[i*nW+j] = ImgIn[(i-1)*nW+j];
+        }
+        else if (i == 0 && j == 0) {
+          ImgTemp[i*nW+j] = ImgIn[i*nW+j];
+        }
+    }
+   }
+
+   for (int i = 0; i < nH; i++) {
+    for (int j = 0; j < nW; j++) {
+        ImgOut[i*nW+j] = (ImgTemp[i*nW+j] - ImgIn[i*nW+j]) + 128;
+    }
+   }
+
+   ecrire_image_pgm(cNomImgEcrite, ImgOut,  nH, nW);
+   free(ImgIn); free(ImgTemp); free(ImgOut);
+
+   return 1;
+}
